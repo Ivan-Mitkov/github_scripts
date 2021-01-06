c389e337bb5c7b53319baed8f1507e2a697e215d
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-01-06 11:33:22 +0200

    add test - see if lint-staged works

--- test/sub.test.js
+++ test/sub.test.js
@@ -1,6 +1,6 @@
 const sub = require("../src/sub");
 
-it("should return correct sub", () => {
+it("should return correct substract", () => {
   const result = sub(5, 2);
   expect(result).toBe(3);
 });
